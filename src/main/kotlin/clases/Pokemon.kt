fde31a3374b7cba18b3e50af96e435e59d6b0127
alejandro <adiabar0510@g.educaand.es>
2023-03-25 18:56:53 +0100

    setter

--- src/main/kotlin/clases/Pokemon.kt
+++ src/main/kotlin/clases/Pokemon.kt
@@ -4,8 +4,17 @@ class Pokemon(tipo: String,Nombre:String,Lore:String,Fuerza:Int,Vida:Int) : Tipo
     var nombre = ""
     var lore = ""
     var fuerza = 0
+        set(value) {
+            if (value<0){
+                println("La fuerza no puede ser negativa")
+            }
+        }
     var vida = 0
-
+        set(value) {
+            if (value<0){
+                println("La vida no puede ser negativa")
+            }
+        }
     init {
         nombre = Nombre
         lore = lore
